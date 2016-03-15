0b2f180046909650b2f974abf76b5cc459deb849
Georgi Ivanov <georgi.iv.ivanov@abv.bg>
2016-03-15 02:10:07 +0200

    User creation
    Tool for the taxer to generate apartment accounts ready

---
+++ js/setup.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+	$(document).on("click", "#create", function(){
+		//alert($('#firstapt').val());
+		//alert($('#lastapt').val());
+		//alert($('#upravitel').val());
+
+		first = $('#firstapt').val();
+		last = $('#lastapt').val();
+		upravitel = $('#upravitel').val();
+
+		for(i = first; i < +last+1; i++){
+			domoupravitel=false;
+			if(i==upravitel){domoupravitel=true;}
+
+			$.ajax("http://localhost:3000/apartments", {
+				method: "POST",
+				contentType: "application/json; charset=utf-8",
+				data: JSON.stringify({
+					apt_number: i,
+					num_of_residents: 0,
+					password: "",
+					domoupravitel: domoupravitel
+				}),
+				dataType: "json"
+			})
+		}
+
+	});
+
+});
